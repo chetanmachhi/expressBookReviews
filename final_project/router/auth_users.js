bfb031b9ab6628bdc517f48d397296ab607243ad
Chetan Machhi <chetanmachhi6@gmail.com>
2025-06-26 17:16:06 +0530

    task 7 done added login functionality with token genration using the config file we created while ago to store secret key

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
+const { SECRET_KEY } = require('../config');
+
 
 let users = [{"username" : "chetan", "password" : "1"}];
 
@@ -12,14 +14,34 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
-
-
+  return users.some(user => user.username === username && user.password === password)
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  try{
+    const username = req.body.username
+    const password = req.body.password
+    if(!username || !password)
+    {
+      return res.status(400).send("provide user and pass!")
+    }
+    else if(!isValid(username))
+    {
+      return res.status(404).send("user dont exist please register!")
+    }
+    else if(!authenticatedUser(username, password))
+    {
+      return res.status(401).send("not correct username and password!")
+    }
+
+    const token = jwt.sign({username}, SECRET_KEY, {expiresIn: "1h"})
+    req.session.token = token
+    return res.status(200).send("user logged in!!!")
+  }catch (error){
+    res.status(500).send("intenal error")
+  }
 });
 
 // Add a book review
